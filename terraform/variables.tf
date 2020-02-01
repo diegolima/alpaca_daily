28253d95d407414035b89d22f76c8d98a38e6d7e
Diego Lima <diego.lima@adtsys.com.br>
2020-02-01 19:14:35 -0300

    Initial commit

---
+++ terraform/variables.tf
@@ -0,0 +1,21 @@
+variable "function_bucket_name"  { type = string }
+variable "function_memory"       {
+    type    = string
+    default = "128"
+}
+variable "function_name"         { type = string }
+variable "function_entrypoint"   { type = string }
+variable "function_runtime"      {
+    type    = string
+    default = "python37"
+}
+variable "function_zip_name"     {
+    type    = string
+    default = "alpaca_daily"
+}
+variable "provider_project_name" { type = string }
+variable "provider_region"       { type = string }
+variable "repository_app_dir"    {
+    type = string
+    default = "app"
+}
